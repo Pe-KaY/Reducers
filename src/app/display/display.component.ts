a85bfcc045f19b4a7033e325ab35fd394928c6c2
Pe-KaY <peekaylink@gmail.com>
2024-08-24 16:12:10 +0000

    fixes double history values

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 import {
@@ -7,7 +7,7 @@ import {
   reset,
   initialValue,
 } from '../store/counter.actions';
-import { addToHistory,clearHistory} from '../store/counterHistory.actions';
+import { addToHistory, clearHistory } from '../store/counterHistory.actions';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../selector/selectors.selector';
@@ -23,6 +23,8 @@ export class DisplayComponent {
   count$!: Observable<number>;
   countHistory$!: Observable<number[]>;
 
+
+  
   constructor(public store: Store<AppState>) {}
 
   ngOnInit() {
@@ -30,27 +32,25 @@ export class DisplayComponent {
     this.countHistory$ = this.store.select('history');
   }
 
-
-
   setInitialNum(event: any): void {
     const target = event.target as HTMLInputElement;
     this.store.dispatch(initialValue({ num: Number(target.value) }));
   }
 
-  
-
   increment() {
     this.store.dispatch(increment());
-    this.store.select('count').subscribe(count => {
+   const subscription = this.store.select('count').subscribe((count) => {
       this.store.dispatch(addToHistory({ count }));
     });
+    subscription.unsubscribe();
   }
 
   decrement() {
     this.store.dispatch(decrement());
-    this.store.select('count').subscribe(count => {
+    const subscription = this.store.select('count').subscribe((count) => {
       this.store.dispatch(addToHistory({ count }));
     });
+    subscription.unsubscribe();
   }
 
   reset() {
